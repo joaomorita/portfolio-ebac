4da313a14f2d536d0c5aded8ed51da1683d796ee
Morita <joao@teste.com>
2024-09-09 19:07:38 -0300

    finalizando a seção "sobre mim"

---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Titulo from '../../components/Titulo'
+
+const Sidebar = () => (
+  <aside>
+    <img src="https://github.com/joaomorita.png" alt="Foto do usuário" />
+    <Titulo fontSize={20}>João Vitor Morita</Titulo>
+  </aside>
+)
+
+export default Sidebar
---
+++ src/containers/Sobre/index.tsx
@@ -0,0 +1,20 @@
+import Titulo from '../../components/Titulo'
+import Paragrafo from '../../components/Paragrafo'
+import { GithubSecao } from './styles'
+
+const Sobre = () => (
+  <section>
+    <Titulo fontSize={16}>Sobre mim</Titulo>
+    <Paragrafo>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos qui autem
+      rem reprehenderit facere dolor quas magnam quos voluptate? Labore qui
+      dolore expedita temporibus itaque, ducimus a eaque necessitatibus sed!
+    </Paragrafo>
+    <GithubSecao>
+      <img src="https://github-readme-stats.vercel.app/api?username=joaomorita&show_icons=true&theme=dracula&include_all_commits=true&count_private=true" />
+      <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=joaomorita&layout=compact&langs_count=7&theme=dracula" />
+    </GithubSecao>
+  </section>
+)
+
+export default Sobre
