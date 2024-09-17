0b51e91ae736f727ce727b96bae0d849925ce7c8
Morita <joao@teste.com>
2024-09-17 17:51:12 -0300

    Estiliza a sidebar

--- src/components/Paragrafo/index.tsx
+++ src/components/Paragrafo/index.tsx
@@ -2,10 +2,19 @@ import { P } from './styles'
 
 export type Props = {
   children: string
+  textAlign?: string
   tipo?: 'principal' | 'secundario'
+  fontSize?: number
 }
-const Paragrafo = ({ children, tipo = 'principal' }: Props) => (
-  <P tipo={tipo}>{children}</P>
+const Paragrafo = ({
+  children,
+  tipo = 'principal',
+  fontSize,
+  textAlign
+}: Props) => (
+  <P textAlign={textAlign} fontSize={fontSize} tipo={tipo}>
+    {children}
+  </P>
 )
 
 export default Paragrafo
--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,22 @@
 import Titulo from '../../components/Titulo'
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
+
+import { Descricao, BotaoTema, SideBarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/joaomorita.png" alt="Foto do usuário" />
-    <Titulo fontSize={20}>João Vitor Morita</Titulo>
+    <SideBarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>João Vitor Morita</Titulo>
+      <Paragrafo textAlign="center" tipo="secundario" fontSize={16}>
+        joaomorita
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Engenheiro front-end
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SideBarContainer>
   </aside>
 )
 
